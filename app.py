f8cbab8fa2d2bf02038d2d5e476a13d8eb333dda
Jerome Amose <jeromeamose1999@gmail.com>
2022-04-13 21:31:20 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pymongo
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
@@ -10,9 +10,12 @@ db = client.ufoDB
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
+    return render_template('index.html')
+
+@app.route("/test")
+def get_results():
     sightings = db.sightings.find()
     poop = []
     for sights in sightings:
@@ -20,7 +23,6 @@ def home():
         poop.append(sights)
     print(poop[0])
     return jsonify(poop)
-
 
 
 if __name__ == '__main__':
